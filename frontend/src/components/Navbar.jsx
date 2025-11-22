44750522784cd809219c751ef04fd4876487da95
sujallgupta <sujalgupta1g@gmail.com>
2025-11-22 11:44:53 +0530

    Formatting

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -46,7 +46,7 @@ const Navbar = () => {
           <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
         </NavLink>
 
-        {/* 🟦 Admin Button */}
+        {/*  Admin Button */}
       <li>
   <a
     href={import.meta.env.VITE_ADMIN_URL}
